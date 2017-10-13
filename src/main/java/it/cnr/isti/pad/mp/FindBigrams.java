c8d87c1758b96390d70c9b1b6a2b1f58b91fcf53
Marco <m.barbone@outlook.it>
2017-10-13 10:51:53 +0200

    Implemented

--- src/main/java/it/cnr/isti/pad/mp/FindBigrams.java
+++ src/main/java/it/cnr/isti/pad/mp/FindBigrams.java
@@ -3,6 +3,7 @@ package it.cnr.isti.pad.mp;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -12,26 +13,53 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 
+import static sun.swing.MenuItemLayoutHelper.max;
+
 /**
  * Hello world!
  *
  */
 public class FindBigrams {
 
+    public static class Mymapper extends Mapper<Object, Text, Text, IntWritable> {
 
-    public static class Mymapper extends Mapper<Object, Text, Text, IntWritable>{
+        private static IntWritable ONE = new IntWritable(1);
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String[] words = value.toString().split(" ");
+            for (int i = 0; i < words.length-1; i++) {
+                value.set(words[i]+ " " +words[i+1]);
+                context.write(value,ONE);
+            }
 
         }
     }
 
-    public static class Myreducer extends Reducer<Text,IntWritable, Text, IntWritable>{
+    public static class Myreducer extends Reducer<Text, IntWritable, LongWritable, Text> {
+
+
+        private static Text string;
+        private static LongWritable max = new LongWritable(-1);
 
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            long tmp = 0;
+            for (IntWritable value : values) {
+                tmp+=1;
+            }
+            if (tmp>max.get()){
+                max.set(tmp);
+                string = key;
+            }
+        }
 
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            context.write(max,string);
+            super.cleanup(context);
         }
     }
+
+
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 
@@ -40,6 +68,7 @@ public class FindBigrams {
 
         job.setMapperClass(Mymapper.class);
         job.setReducerClass(Myreducer.class);
+        job.setNumReduceTasks(1);
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
@@ -48,6 +77,7 @@ public class FindBigrams {
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
 
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 
-        System.exit(job.waitForCompletion(true) ? 0:1);
 }
