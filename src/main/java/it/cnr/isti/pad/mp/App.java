05cbf562917bee6a2b776e291ae09fcd7ea3e512
Marco <m.barbone@outlook.it>
2017-10-13 10:26:19 +0200

    fist commmit

---
+++ src/main/java/it/cnr/isti/pad/mp/App.java
@@ -0,0 +1,13 @@
+package it.cnr.isti.pad.mp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
